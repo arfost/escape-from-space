543881a1e93bc162b5e75e5be2a768242aa4dcc7
arfost <chevalier.frugier@gmail.com>
2019-05-30 19:37:58 +0200

    move chest

--- src/Entities/Game.js
+++ src/Entities/Game.js
@@ -1,5 +1,6 @@
-
-import { FireReference } from '../../futur-lib/data.js'
+import {
+    FireReference
+} from '../../futur-lib/data.js'
 import * as firebase from 'firebase/app';
 import 'firebase/functions';
 
@@ -17,76 +18,87 @@ export class Game extends FireReference {
             cells: "cells/" + this.id,
         };
     }
-    get actions(){
+    get actions() {
         return {
             launchGame: async (key) => {
                 const res = await firebase.functions().httpsCallable('launchGame')(key);
             },
             moveChar: (oldRoomId, roomId, uid) => {
-                if(this.data.game.players[this.data.game.gameInfo.toPlay].uid !== uid){
+                if (this.data.game.players[this.data.game.gameInfo.toPlay].uid !== uid) {
                     throw new Error('It\'s not your turn');
                 }
 
+                //for each char living, change position if the one we move
                 this.data.game.liveChars = this.data.game.liveChars.map(char => {
                     if(char.pos == oldRoomId){
                         char.pos = roomId;
                     }
                     return char;
                 });
-                this.data.game.gameInfo.turn++;
-                this.data.game.gameInfo.toPlay++;
-                if(this.data.game.gameInfo.toPlay>=this.data.game.players.length){
-                    this.data.game.gameInfo.toPlay = 0;
+
+                //if chest is in the room, move it with char
+                let oldRoom = this.data.cells.filter(cell => cell.id == oldRoomId);
+                if (oldRoom.chest) {
+                    oldRoom.chest = false;
+                    let newRoom = this.data.cells.filter(cell => cell.id == roomId);
+                    newRoom.chest = true;
                 }
+
+
+                this.data.game = this.passTurn(this.data.game);
                 this.save();
             },
             killChar: (char, uid) => {
-                if(this.data.game.players[this.data.game.gameInfo.toPlay].uid !== uid){
+                if (this.data.game.players[this.data.game.gameInfo.toPlay].uid !== uid) {
                     throw new Error('It\'s not your turn');
                 }
 
-                this.data.game.liveChars = this.data.game.liveChars.filter(c=>c.id!==char.id);
-                
+                this.data.game.liveChars = this.data.game.liveChars.filter(c => c.id !== char.id);
+
 
                 this.data.game.deadChars = this.data.game.deadChars ? [...this.data.game.deadChars, char] : [char];
 
-                if(this.data.game.liveChars.length === 1){
+                if (this.data.game.liveChars.length === 1) {
                     this.data.game = this.finishGame(this.data.game);
-                }else{
-                    this.data.game.gameInfo.turn++;
-                    this.data.game.gameInfo.toPlay++;
-                    if(this.data.game.gameInfo.toPlay>=this.data.game.players.length){
-                        this.data.game.gameInfo.toPlay = 0;
-                    }
+                } else {
+                    this.data.game = this.passTurn(this.data.game);
                 }
                 this.save();
             }
         }
     }
 
-    finishGame(game){
+    passTurn(game) {
+        game.gameInfo.turn++;
+        game.gameInfo.toPlay++;
+        if (game.gameInfo.toPlay >= game.players.length) {
+            game.gameInfo.toPlay = 0;
+        }
+    }
+
+    finishGame(game) {
         game.deadChars.push(game.liveChars.shift());
 
-        if(game.score){
-            for(let player in game.score){
+        if (game.score) {
+            for (let player in game.score) {
                 game.score[player].total += game.score[player].manche;
                 game.score[player].manche = 0;
             }
-        }else{
+        } else {
             game.score = {}
-            for(let player of game.players){
+            for (let player of game.players) {
                 game.score[player.name] = {
-                    manche:0,
-                    total:0
+                    manche: 0,
+                    total: 0
                 }
             }
         }
         let done = 0;
         let deadCharsCopy = [...game.deadChars]
-        while(done < game.players.length){
+        while (done < game.players.length) {
             let char = deadCharsCopy.pop();
-            for(let player of game.players){
-                if(player.chars.includes(char.id) && game.score[player.name].manche === 0){
+            for (let player of game.players) {
+                if (player.chars.includes(char.id) && game.score[player.name].manche === 0) {
                     game.score[player.name].manche = deadCharsCopy.length;
                     done++;
                 }
@@ -96,26 +108,29 @@ export class Game extends FireReference {
         return game;
     }
 
-    formatDatas({game, cells}) {
+    formatDatas({ game, cells }) {
         let data = {
-            deadChars : game.deadChars || [],
-            liveChars : game.liveChars || [],
-            cells : cells || [],
-            players : game.players || [],
-            gameInfo : game.gameInfo || {
+            deadChars: game.deadChars || [],
+            liveChars: game.liveChars || [],
+            cells: cells || [],
+            players: game.players || [],
+            gameInfo: game.gameInfo || {
                 turn: 1,
-                toPlay:0,
-                votes:0
+                toPlay: 0,
+                votes: 0
             },
             ready: game.ready,
             score: game.score,
-            loaded:game.loaded,
+            loaded: game.loaded,
             finished: game.finished
         };
         return data;
     }
 
-    presave({game, cells}) {
+    presave({
+        game,
+        cells
+    }) {
         let data = {
             game,
             cells
@@ -125,10 +140,10 @@ export class Game extends FireReference {
 
     get defaultValues() {
         return {
-            game:{
-                loaded:false,
+            game: {
+                loaded: false,
             },
-            cells:[]
+            cells: []
         };
     }
 }
