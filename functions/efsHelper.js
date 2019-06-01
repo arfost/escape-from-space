22097c4fdf5ce0b50dc0f07ffe3c8c26ebd1fbcd
arfost <chevalier.frugier@gmail.com>
2019-06-02 00:06:59 +0200

    random dungeon generation

--- functions/efsHelper.js
+++ functions/efsHelper.js
@@ -4,39 +4,110 @@ const EO = 'EO';
 
 class DungeonGenerator {
 
-    getNewDungeon(size){
+    getNewDungeon(size) {
         this.rooms = [];
 
         let activeRoom = this.getNextRoom();
         this.rooms.push(activeRoom);
 
-        while(this.rooms.length<size){
+        /*
+        let connection = {
+            orientationConstraint:1,
+            direction:'N'
+        }
+
+        let newRoom = this.getNextRoom(activeRoom, connection);
+                
+        activeRoom.connections.rooms.push(newRoom.id);
+        activeRoom.connections.directions.push(connection.direction);
+
+        newRoom.connections.rooms.push(activeRoom.id);
+        newRoom.connections.directions.push(this.inverseDirection(connection.direction));
+
+        //activeRoom = newRoom;
+        this.rooms.push(newRoom);
+
+        console.log(this.findNextConnection(activeRoom))
+        */
+        
+        while (this.rooms.length < size) {
             let connection = this.findNextConnection(activeRoom);
-            if(connection){
+            if (connection) {
                 
                 let newRoom = this.getNextRoom(activeRoom, connection);
-                if(this.isOnAnotherRoom(this.rooms, newRoom)){
-                    let parent = this.rooms.find(room=>room.id === activeRoom.id -1);
-                    activeRoom = parent ? parent : activeRoom;
-                    continue;
-                }
+                
                 activeRoom.connections.rooms.push(newRoom.id);
                 activeRoom.connections.directions.push(connection.direction);
 
                 newRoom.connections.rooms.push(activeRoom.id);
                 newRoom.connections.directions.push(this.inverseDirection(connection.direction));
 
+                this.addConnections(this.rooms, newRoom);
+
                 activeRoom = newRoom;
                 this.rooms.push(activeRoom);
-            }else{
-                let parent = this.rooms.find(room=>room.id === activeRoom.id -1);
-                activeRoom = parent ? parent : this.rooms[this.rooms.length-1];
+            } else {
+                let parent = this.rooms.find(room => room.id === activeRoom.id - 1);
+                activeRoom = parent ? parent : this.rooms[this.rooms.length - 1];
+            }
+        }
+        
+        
+        this.rooms[this.rooms.length - 1].chest = true;
+        console.log(JSON.stringify(this.rooms, null, 4))
+
+        return this.rooms;
+    }
+
+    addConnections(rooms, newRoom) {
+
+        let posNordNS = this.createPosForRoom(newRoom, 'N', NS);
+        let posNordEO = this.createPosForRoom(newRoom, 'N', EO);
+
+        let posSouthNS = this.createPosForRoom(newRoom, 'S', NS);
+        let posSouthEO = this.createPosForRoom(newRoom, 'S', EO);
+
+        let posWestNS = this.createPosForRoom(newRoom, 'O', NS);
+        let posWestEO = this.createPosForRoom(newRoom, 'O', EO);
+
+
+        let posEastNS = this.createPosForRoom(newRoom, 'E', NS);
+        let posEastEO = this.createPosForRoom(newRoom, 'E', EO);
+
+        for (let room of rooms) {
+            if ((room.pos.x === posNordNS.x && room.pos.y === posNordNS.y) || (room.pos.x === posNordEO.x && room.pos.y === posNordEO.y)) {
+                room.connections.directions.push('S');
+                room.connections.rooms.push(newRoom.id);
+
+                newRoom.connections.directions.push('N');
+                newRoom.connections.rooms.push(room.id);
+            }
+
+            if ((room.pos.x === posSouthNS.x && room.pos.y === posSouthNS.y) || (room.pos.x === posSouthEO.x && room.pos.y === posSouthEO.y)) {
+                room.connections.directions.push('N');
+                room.connections.rooms.push(newRoom.id);
+
+                newRoom.connections.directions.push('S');
+                newRoom.connections.rooms.push(room.id);
+            }
+
+            if ((room.pos.x === posWestNS.x && room.pos.y === posWestNS.y) || (room.pos.x === posWestEO.x && room.pos.y === posWestEO.y)) {
+                room.connections.directions.push('E');
+                room.connections.rooms.push(newRoom.id);
+
+                newRoom.connections.directions.push('O');
+                newRoom.connections.rooms.push(room.id);
+            }
+
+            if ((room.pos.x === posEastNS.x && room.pos.y === posEastNS.y) || (room.pos.x === posEastEO.x && room.pos.y === posEastEO.y)) {
+                room.connections.directions.push('O');
+                room.connections.rooms.push(newRoom.id);
+
+                newRoom.connections.directions.push('E');
+                newRoom.connections.rooms.push(room.id);
             }
         }
 
-        this.rooms[this.rooms.length-1].chest = true;
-        console.log(JSON.stringify(this.rooms, null, 4))
-        return this.rooms;
     }
 
     inverseDirection(direction){
@@ -46,28 +117,32 @@ class DungeonGenerator {
             case 'E': return 'O';
             case 'O': return 'E';
         }
-        throw new Error("unknow direction ", )
+        throw new Error("unknow direction "+direction)
     }
 
     getSquarePos(pos, orientation){
         return {
             x:pos.x,
             y:pos.y,
-            xMax:pos.x+(orientation === NS ? 2 : 4),
-            yMax:pos.y+(orientation === EO ? 2 : 4)
+            xMax:pos.x+(orientation === NS ? 1 : 3),
+            yMax:pos.y+(orientation === EO ? 1 : 3)
         }
     }
 
     isOnAnotherRoom(rooms, newRoom){
         let rect2 = this.getSquarePos(newRoom.pos, newRoom.orientation);
         for(let room of rooms){
-            let rect1 = this.getSquarePos(room.pos, room.orientation);
-            if(rect1.x < rect2.xMax &&
-                rect1.xMax > rect2.x &&
-                rect1.y < rect2.yMax &&
-                rect1.yMax > rect2.y){
+            let rect1 = this.getSquarePos(room.pos, room.orientation);          
+            if(rect1.x <= rect2.xMax &&
+                rect1.xMax >= rect2.x &&
+                rect1.y <= rect2.yMax &&
+                rect1.yMax >= rect2.y ) {
                     return true;
-                }
+            }
+            
+            if (rect2.x < 1 || rect2.xMax > 15 || rect2.y < 1 || rect2.yMax > 15) {
+                return true;
+            }
         }
         return false;
     }
@@ -77,45 +152,123 @@ class DungeonGenerator {
         let possible = [];
         
         //test north
-        let posNordNS = this.createPosForRoom(room, 'N', )
-        if(room.pos.y-2 > 0 && !room.connections.directions.includes('N')){
+        console.log('pouet')
+        let posNordNS = this.createPosForRoom(room, 'N', NS);
+        let canNS = !this.isOnAnotherRoom(this.rooms, { pos: posNordNS, orientation: NS });
+
+        let posNordEO = this.createPosForRoom(room, 'N', EO);
+        let canEO = !this.isOnAnotherRoom(this.rooms, { pos: posNordEO, orientation: EO });
+
+        if (canNS && !canEO) {
             possible.push({
-                orientationConstraint:room.pos.y-4>0 ? undefined: 2,
+                orientationConstraint:1,
+                direction:'N'
+            })
+        }
+
+        if (!canNS && canEO) {
+            possible.push({
+                orientationConstraint:2,
+                direction:'N'
+            })
+        }
+
+        if (canEO && canNS) {
+            possible.push({
                 direction:'N'
             })
         }
 
         //test south
-        if(room.pos.y+2 < 15 && !room.connections.directions.includes('S')){
+        console.log('pouet')
+        let posSouthNS = this.createPosForRoom(room, 'S', NS);
+        canNS = !this.isOnAnotherRoom(this.rooms, { pos: posSouthNS, orientation: NS });
+
+        let posSouthEO = this.createPosForRoom(room, 'S', EO);
+        canEO = !this.isOnAnotherRoom(this.rooms, { pos: posSouthEO, orientation: EO });
+
+        if (canNS && !canEO) {
             possible.push({
-                orientationConstraint:room.pos.y+4 < 15 ? undefined: 2,
+                orientationConstraint:1,
+                direction:'S'
+            })
+        }
+
+        if (!canNS && canEO) {
+            possible.push({
+                orientationConstraint:2,
+                direction:'S'
+            })
+        }
+
+        if (canEO && canNS) {
+            possible.push({
                 direction:'S'
             })
         }
 
         //test ouest
-        if(room.pos.x-2 > 1 && !room.connections.directions.includes('O')){
+        console.log('pouet')
+        let posWestNS = this.createPosForRoom(room, 'O', NS);
+        canNS = !this.isOnAnotherRoom(this.rooms, { pos: posWestNS, orientation: NS });
+
+        let posWestEO = this.createPosForRoom(room, 'O', EO);
+        canEO = !this.isOnAnotherRoom(this.rooms, { pos: posWestEO, orientation: EO });
+
+        if (canNS && !canEO) {
             possible.push({
-                orientationConstraint:room.pos.y-4 > 1 ? undefined: 1,
+                orientationConstraint:1,
+                direction:'O'
+            })
+        }
+
+        if (!canNS && canEO) {
+            possible.push({
+                orientationConstraint:2,
+                direction:'O'
+            })
+        }
+
+        if (canEO && canNS) {
+            possible.push({
                 direction:'O'
             })
         }
 
         //test east
-        if(room.pos.x+2 < 15 && !room.connections.directions.includes('E')){
+        console.log('pouet')
+        let posEastNS = this.createPosForRoom(room, 'E', NS);
+        canNS = !this.isOnAnotherRoom(this.rooms, { pos: posEastNS, orientation: NS });
+
+        let posEastEO = this.createPosForRoom(room, 'E', EO);
+        canEO = !this.isOnAnotherRoom(this.rooms, { pos: posEastEO, orientation: EO });
+
+        if (canNS && !canEO) {
             possible.push({
-                orientationConstraint:room.pos.y+4 < 15 ? undefined: 1,
+                orientationConstraint:1,
                 direction:'E'
             })
         }
 
+        if (!canNS && canEO) {
+            possible.push({
+                orientationConstraint:2,
+                direction:'E'
+            })
+        }
+
+        if (canEO && canNS) {
+            possible.push({
+                direction:'E'
+            })
+        }
         return possible.find(p=>getDice(1,100)<25);
     }
 
     getNextRoom(connectedRomm, link){
 
         let orientation = this.getOrientation(link ? link.orientationConstraint : undefined);
-        let pos = connectedRomm ? this.createPosForRoom(connectedRomm, link.direction, connectedRomm.orientation, orientation) : {x:7, y:7}
+        let pos = connectedRomm ? this.createPosForRoom(connectedRomm, link.direction, orientation) : {x:7, y:7}
 
         return {
             id: this.rooms.length+1,
@@ -147,6 +300,7 @@ class DungeonGenerator {
             if(orientation === EO){
                 if(selfOrientation === NS){
                     x += 1;
+                    y -= 2;
                 }
                 y -= 2;
             }
@@ -185,7 +339,8 @@ class DungeonGenerator {
         if(direction === 'O'){
             if(orientation === NS){
                 if(selfOrientation === EO){
-                    y -= 1;
+                    y += 1;
+                    x -= 2;
                 }
                 x -= 2;
             }
--- src/views/efs-game.js
+++ src/views/efs-game.js
@@ -217,7 +217,7 @@ class EfsGame extends EfsBase {
         return html`<div 
                         @click="${e=>this.roomClicked(room)}"
                         class="room ${room.orientation} flex-box f-j-center f-a-center scroll ${room.orientation === 'NS' ? 'f-vertical' : 'f-horizontal'} ${(this.selectable && this.selectable.includes(room.id)) ? 'selectable' : ''}" 
-                        style="grid-column:${room.pos.x}/span ${room.orientation === 'NS' ? 2 : 4};grid-row:${room.pos.y}/span ${room.orientation === 'NS' ? 4 : 2}">
+                        style="grid-column:${room.pos.x+1}/span ${room.orientation === 'NS' ? 2 : 4};grid-row:${room.pos.y+1}/span ${room.orientation === 'NS' ? 4 : 2}">
                             ${charsInRoom.map(char => this.makeChar(char))}
                             ${room.chest ? html`<div class="chest" @mouseover="${e=>this.configureTooltip(CHESTDESC)}"></div>` : ``}
                             ${room.exit ? html`<div class="exit ${room.chest && room.exit ? 'selectable' : ''}" @mouseover="${e=>this.configureTooltip(EXITGATE)}"></div>` : ``}
